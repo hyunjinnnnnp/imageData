2ecc3e46c6b5c7e73ee462b734ceec8fa58e1fe9
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-11-28 19:12:26 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+let canPlay;
+let applyEffect;
+
+const canvas = document.getElementById("Canvas");
+const context = canvas.getContext("2d");
+const startButton = document.getElementById("StartButton");
+const toggleButton = document.getElementById("ToggleButton");
+const video = document.getElementById("SourceVideo");
+
+function init() {
+  video.muted = true;
+
+  if (video.readyState >= 3) {
+    readyToPlay();
+  } else {
+    video.addEventListener("canplay", readyToPlay);
+  }
+
+  startButton.addEventListener("click", function () {
+    console.log(canPlay);
+    if (!canPlay) return;
+    // Play video
+    video.play();
+
+    drawFrame(video);
+  });
+
+  toggleButton.addEventListener("click", function () {
+    applyEffect = !applyEffect;
+  });
+}
+
+function readyToPlay() {
+  // Set the canvas the same width and height of the video
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  canPlay = true;
+}
+// canvas.addEventListener("move", (e) => {
+
+// const x = e.layerX;
+// const y = e.layerY;
+// });
+function drawFrame(video) {
+  context.drawImage(video, 0, 0);
+
+  if (applyEffect) {
+    let imageData = context.getImageData(
+      0,
+      0,
+      (canvas.width * 2) / 3,
+      canvas.height
+    );
+
+    invertColors(imageData.data);
+
+    context.putImageData(imageData, 0, 0);
+  }
+
+  setTimeout(function () {
+    drawFrame(video);
+  }, 10);
+}
+
+function invertColors(data) {
+  for (let i = 0; i < data.length; i += 4) {
+    // let r = data[i];
+    // let g = data[i + 1];
+    // let b = data[i + 2];
+    // let brightness = (2 * r + 4 * g + b) >>> 3;
+    // data[i] = brightness;
+    // data[i + 1] = brightness;
+    // data[i + 2] = brightness;
+    data[i] = data[i] ^ 55; // Invert Red
+    data[i + 1] = data[i + 1] ^ 255; // Invert Green
+    data[i + 2] = data[i + 2] ^ 255; // Invert Blue
+  }
+}
+
+window.addEventListener("load", init);
